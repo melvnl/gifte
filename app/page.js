777b9f29195f2ada9bee70df5ab250f1ca7a0b44
melvnl <melvinlim2000@gmail.com>
2023-09-06 01:16:49 +0700

    fix

--- app/page.js
+++ app/page.js
@@ -130,7 +130,7 @@ export default function Home() {
 
 
   return (
-    <div ref={ref} className={classNames(visible === 1 ? " " : " h-screen overflow-hidden", " no-scrollbar")}>
+    <div ref={ref} className={classNames(visible === 1 ? " " : " h-screen overflow-hidden touch-none", " no-scrollbar")}>
       <AnimatePresence mode='wait'>
         {visible === 0 && <Homepage />}
         {visible === 1 && <Homepage2 />}
