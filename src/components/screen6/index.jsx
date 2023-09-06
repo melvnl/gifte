19d1daff598542a98c744d7d5e2335819b6e2637
melvnl <melvinlim2000@gmail.com>
2023-09-06 22:52:01 +0700

    update: typewriter

--- src/components/screen6/index.jsx
+++ src/components/screen6/index.jsx
@@ -3,32 +3,59 @@
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import Header from '../header';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typewriter } from 'react-simple-typewriter'
 
 const leftCol = [
-  "The Garden of Glow mini collection draws inspiration from the concept of inner beauty. Each product within this collection is designed with a focus on simplicity, both in its arrangement and packaging. We embrace texture and intricate visual details to elevate your sensory experience",
+  " mini collection draws inspiration from the concept of inner beauty. Each product within this collection is designed with a focus on simplicity, both in its arrangement and packaging. We embrace texture and intricate visual details to elevate your sensory experience",
 ]
 
 export default function Screen6() {
+
+  const [start, setStart] = useState(false);
 
   const { ref, inView, entry } = useInView({
     threshold: 0.75,
     triggerOnce: true
   });
 
+  useEffect(() => {
+    if (!start) {
+      setTimeout(() => {
+        setStart(true);
+      }, 300); // Adjust the delay time as needed
+    }
+  }, [start]);
+
   return (
     <div className=' '>
       <Header />
-      <div ref={ref} className=" mt-[200px] flex flex-col text-gray items-center leading-5 text-sm gap-4 px-5 py-3">
-        <Typewriter
-          words={leftCol}
-          loop={1}
-          cursor
-          cursorStyle=''
-          typeSpeed={15}
-          deleteSpeed={1000000}
-        />
+      <div ref={ref} className=" mt-[190px] text-gray leading-5 text-sm gap-4 px-5 py-3">
+        <span className=" font-RecklessNueu font-medium italic w-fit">
+          <Typewriter
+            words={["The Garden Of Glow"]}
+            loop={1}
+            cursor
+            cursorStyle=''
+            typeSpeed={15}
+            onLoopDone={() => {
+              console.log("!@3")
+              setStart(true)
+            }}
+          />
+        </span>
+        <span>
+          {start && (<Typewriter
+            words={leftCol}
+            loop={1}
+            cursor
+            cursorStyle=''
+            typeSpeed={15}
+            onLoopDone={() => { console.log("taik") }}
+          // deleteSpeed={0}
+          />)}
+        </span>
+
       </div>
     </div>
   )
